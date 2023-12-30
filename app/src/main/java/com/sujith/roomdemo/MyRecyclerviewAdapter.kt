119d62944c8ef7b37a374ffc6436560320957a4c
sujithmanjavana <sujithmanjavana@gmail.com>
2023-12-30 15:49:46 +0530

    add recyclerview adapter

---
+++ app/src/main/java/com/sujith/roomdemo/MyRecyclerviewAdapter.kt
@@ -0,0 +1,33 @@
+package com.sujith.roomdemo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.sujith.roomdemo.databinding.ListItemBinding
+import com.sujith.roomdemo.db.Subscriber
+
+class MyRecyclerviewAdapter(private val subscribers: List<Subscriber>) :
+    RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ListItemBinding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.list_item, parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return subscribers.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(subscribers[position])
+    }
+}
+
+class MyViewHolder(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(subscriber: Subscriber) {
+        binding.nameTxt.text = subscriber.name
+        binding.emailTxt.text = subscriber.email
+    }
+}
